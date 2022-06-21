1f1306d642ea07b1f34d4309ee4dabb6e19652dd
Andres Catano <andresfcatano0@gmail.com>
2022-06-21 15:26:26 -0400

    Add scroll component

---
+++ src/Scroll.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+ const Scroll = (props) => {
+   return (
+     // Double curly brakets for css styles
+     <div style={{ overflowY: 'scroll', borderTop: "1px solid white", height: '600px' }}>
+      {props.children}
+     </div>
+   );
+ };
+
+
+export default Scroll; 
