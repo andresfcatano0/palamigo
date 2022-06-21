703be8578ca60ccca174a25b33e5af9f8f4419e4
Andres Catano <andresfcatano0@gmail.com>
2022-06-21 13:08:35 -0400

    Use jsonplaceholder to fetch user info

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React, {Component} from 'react';
 import CardList from "./CardList";
 import SearchBox from "./SearchBox"
-// robots file is not using export default, so destructuring {} must be used because 
-// the file could have multiple exports.   
-import { robots } from "./robots";
 import "./App.css"
 
 class App extends Component {  
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ""
     }
   }
   
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users })); 
+  }
+
   onSearchChange = (event) => {
     // Updates the state of search field with whatever is typed
     this.setState({searchfield: event.target.value})
