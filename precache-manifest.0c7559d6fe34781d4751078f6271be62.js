d08b3a7d43461e7f8161407ee84e4200b6aca391
Andres Catano <andresfcatano0@gmail.com>
2022-06-28 01:55:21 -0400

    Updates

---
+++ precache-manifest.0c7559d6fe34781d4751078f6271be62.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b139065dcc7b31b8f330",
+    "url": "/palamigo/static/css/main.b1571c49.chunk.css"
+  },
+  {
+    "revision": "b139065dcc7b31b8f330",
+    "url": "/palamigo/static/js/main.b139065d.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/palamigo/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "859c9085a9dc80c12625",
+    "url": "/palamigo/static/css/2.79c1c98f.chunk.css"
+  },
+  {
+    "revision": "859c9085a9dc80c12625",
+    "url": "/palamigo/static/js/2.859c9085.chunk.js"
+  },
+  {
+    "revision": "252403a92dc1070e104b",
+    "url": "/palamigo/static/js/runtime~main.252403a9.js"
+  },
+  {
+    "revision": "acc68e52cc0106d46c7f1d0c8a69be34",
+    "url": "/palamigo/static/media/ultraviolentbb_reg.acc68e52.woff"
+  },
+  {
+    "revision": "18ce62b6cb8b3b2d6bb44a9a9e27e954",
+    "url": "/palamigo/index.html"
+  }
+];
