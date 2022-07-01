7c04574cb45e7b8f6399f9bc93091ae7012e41be
Andres Catano <andresfcatano0@gmail.com>
2022-07-01 16:03:25 -0400

    change SearchBox component styling

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,7 +4,7 @@ const SearchBox = ({searchChange}) => {
   return(
     <div className='pa2'>
       <input 
-        className='pa3 ba b--green bg-lightest-blue'
+        className='pa3 ba b-purple bg-lightest-purple'
         type="search" 
         placeholder='Seach friends' 
         onChange= {searchChange}
